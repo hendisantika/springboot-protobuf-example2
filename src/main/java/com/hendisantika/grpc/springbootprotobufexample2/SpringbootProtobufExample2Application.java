9304389bebd47dba4327502b56aa241c5530fbfe
Hendi Santika <hendi.santika@allianz.co.id>
2019-04-07 09:37:24 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/grpc/springbootprotobufexample2/SpringbootProtobufExample2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.grpc.springbootprotobufexample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProtobufExample2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProtobufExample2Application.class, args);
+    }
+
+}
