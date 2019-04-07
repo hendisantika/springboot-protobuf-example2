76831949c7275decaa5ed8c9b8d5150682db1508
hendisantika <hendisantika@yahoo.co.id>
2019-04-07 10:00:06 +0700

    #10 - Add CustomerRepository

---
+++ src/main/java/com/hendisantika/grpc/springbootprotobufexample2/repository/CustomerRepository.java
@@ -0,0 +1,18 @@
+package com.hendisantika.grpc.springbootprotobufexample2.repository;
+
+import com.hendisantika.grpc.springbootprotobufexample2.proto.CustomerProtos;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-protobuf-example2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-04-07
+ * Time: 09:56
+ */
+public interface CustomerRepository {
+    CustomerProtos.Customer findById(int id);
+
+    CustomerProtos.CustomerList findAll();
+}
